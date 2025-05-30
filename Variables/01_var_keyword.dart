0dcf06945b2b9eadc4028c2527874b6fe2603960
CHEGEBB <chegephil24@gmail.com>
2025-05-30 15:33:56 +0300

    Add examples demonstrating the usage of 'var' in Dart and include a hello world program

---
+++ Variables/01_var_keyword.dart
@@ -0,0 +1,11 @@
+// Created by CHEGEBB
+// This file demonstrates how to use 'var' in Dart
+
+void main() {
+  // 'var' lets Dart decide the type
+  var name = 'Brian';  // Dart infers it as String
+  var age = 20;        // Dart infers it as int
+
+  print('Name: $name');
+  print('Age: $age');
+}
---
+++ Variables/hello_world.dart
@@ -0,0 +1,3 @@
+void main(){
+    print('Hello World  of Dart Be kind to us kindly 🙇');
+}
