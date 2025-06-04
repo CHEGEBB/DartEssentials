ebe51741b4a21255c5e059284830aa8548dae997
CHEGEBB <chegephil24@gmail.com>
2025-06-04 11:44:29 +0300

    Refactor greetCustomer function to accept a name parameter and update greeting messages; add new function greetUser for personalized greetings.

---
+++ Functions/02_function_with_parameters.dart
@@ -0,0 +1,10 @@
+// Created by CHEGEBB
+// Function with parameters that prints a personalized greeting
+
+void greetUser(String name) {
+  print('Hello, $name!');
+}
+
+void main() {
+  greetUser('Brian');
+}
--- Functions/trial_function_barista.dart
+++ Functions/trial_function_barista.dart
@@ -1,18 +1,18 @@
-void greetCustomer(){
-  double time = 12.05;
+void greetCustomer(String name){
+  double time = 21.05;
 
   if(time <= 12.00){
-    print('Good Morning🌄🌄🌅, welcome to Madcafe, how can i serve you today');
+    print('Good Morning🌄🌄🌅 , $name welcome to Madcafe, how can i serve you today');
   }
   else if(time >= 12.00 && time <= 16.00){
-    print('Good Afternoon ☀️☀️🌞, welcome to Madcafe, how can i serve you today');
+    print('Good Afternoon ☀️☀️🌞, $name welcome to Madcafe, how can i serve you today');
   }
   else{
-    print('Good Evening 🌃🌃🌆, welcome to Madcafe, how can i serve you today');
+    print('Good Evening 🌃🌃🌆, $name  welcome to Madcafe, how can i serve you today');
   }
 
 }
 
 void main (){
-  greetCustomer();
+  greetCustomer('Brian');
 }
