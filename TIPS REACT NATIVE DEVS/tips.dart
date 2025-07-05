a4477e5e9f358239d9f985fbcf5e5df6c016a4b3
CHEGEBB <chegephil24@gmail.com>
2025-07-05 13:58:45 +0300

    Add example for fetching IP address using HTTP in Dart

--- TIPS REACT NATIVE DEVS/tips.dart
+++ TIPS REACT NATIVE DEVS/tips.dart
@@ -107,3 +107,32 @@ void main() {
 // }
 
 // main();
+
+
+// Dart
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Example {
+  Future<String> _getIPAddress() async {
+    final url = Uri.https('httpbin.org', '/ip');
+    final response = await http.get(url);
+    final ip = jsonDecode(response.body)['origin'] as String;
+    return ip;
+  }
+}
+
+/// An async function returns a `Future`.
+/// It can also return `void`, unless you use
+/// the `avoid_void_async` lint. In that case,
+/// return `Future<void>`.
+void main() async {
+  final example = Example();
+  try {
+    final ip = await example._getIPAddress();
+    print(ip);
+  } catch (error) {
+    print(error);
+  }
+}
